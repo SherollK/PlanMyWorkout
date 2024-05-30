3b09f70ea53fc4c2734d1a69047de0392f1f50cf
Karunagaran Sheroll <sherollk02@gmail.com>
2024-05-30 21:58:01 +0530

    Dark Mode Test

---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/theme/theme.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if(_themeData == lightMode) {
+      _themeData = darkMode;
+    } else {
+      _themeData = lightMode;
+    }
+    notifyListeners(); 
+  }
+}
