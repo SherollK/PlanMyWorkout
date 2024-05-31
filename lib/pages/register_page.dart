3d280c229d2ec732b1a070fb29eccade636ba75f
Karunagaran Sheroll <sherollk02@gmail.com>
2024-05-31 14:56:43 +0530

    Fixing register page overflow error

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -57,90 +57,94 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Icon(
-              Icons.fitness_center,
-              size: 90,
-              color: Theme.of(context).colorScheme.primary,
-            ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-            const SizedBox(height: 20),
-          
-            //welcome message
-            Text(
-              "Ready to begin? Register now!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
+              const SizedBox(height: 150),
+              //logo
+              Icon(
+                Icons.fitness_center,
+                size: 90,
                 color: Theme.of(context).colorScheme.primary,
-                fontSize: 14,               
               ),
-            ),
-
-            const SizedBox(height: 25),
         
-            //email textfield
-            MyTextField(
-              hintText: "Email",
-              obscureText: false,
-              controller: _emailController
-            ),
-
-            const SizedBox(height: 10),
-        
-            //password textfield
-            MyTextField(
-              hintText: "Password",
-              obscureText: true,
-              controller: _passwordController,
-            ),
-
-            const SizedBox(height: 10),
-
-            //confirm password textfield
-            MyTextField(
-              hintText: "Password",
-              obscureText: true,
-              controller: _confirmPasswordController,
-            ),
-
-            const SizedBox(height: 25),
-        
-            //register button
-            MyButton(
-              text: "Register",
-              onTap: () => register(context),
-            ),
+              const SizedBox(height: 20),
             
-            const SizedBox(height: 25),
-
-            //register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account? ",
-                  style: 
-                    TextStyle(color: Theme.of(context).colorScheme.primary),
+              //welcome message
+              Text(
+                "Ready to begin? Register now!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 14,               
                 ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Text(
-                    "Login Now",
+              ),
+        
+              const SizedBox(height: 25),
+          
+              //email textfield
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: _emailController
+              ),
+        
+              const SizedBox(height: 10),
+          
+              //password textfield
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: _passwordController,
+              ),
+        
+              const SizedBox(height: 10),
+        
+              //confirm password textfield
+              MyTextField(
+                hintText: "Confirm Password",
+                obscureText: true,
+                controller: _confirmPasswordController,
+              ),
+        
+              const SizedBox(height: 25),
+          
+              //register button
+              MyButton(
+                text: "Register",
+                onTap: () => register(context),
+              ),
+              
+              const SizedBox(height: 25),
+        
+              //register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account? ",
                     style: 
-                      TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).colorScheme.primary),
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
                   ),
-                ),
-              ],
-            ),
-
-
-          ],
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Login Now",
+                      style: 
+                        TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                  ),
+                ],
+              ),
+        
+        
+            ],
+          ),
         ),
       )
     );
