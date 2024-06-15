fe552c29aea2c8171be4fe4c4b25420a89eef0e5
Karunagaran Sheroll <sherollk02@gmail.com>
2024-06-15 21:29:40 +0530

    Weather page UI

---
+++ lib/const.dart
@@ -0,0 +1,1 @@
+const OPENWEATHER_API_KEY = "cae37dd1ec8cea4b875f9f6c10f1d9a0";
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,20 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/const.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/weather.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final WeatherFactory _weatherFactory = WeatherFactory(OPENWEATHER_API_KEY);
+
+  Weather? _weather;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherFactory.currentWeatherByCityName("Colombo").then((w) {
+      setState(() {
+        _weather = w;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        titleTextStyle: TextStyle(fontSize: 18,color: Theme.of(context).colorScheme.primary),
-        title: const Text("Weather"),       
+      appBar: AppBar(      
         backgroundColor: Colors.transparent,
         elevation: 0,
         foregroundColor: Theme.of(context).colorScheme.primary,
-        centerTitle: true,
       ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_weather == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _locationHeader(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.08),
+          _dateTimeInfo(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+          _weatherIcon(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+          _currentTemp(),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.09),
+        ],
+      ),
+    );
+  }
+
+  Widget _locationHeader() {
+    return Text(_weather?.areaName ?? "",
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500));
+  }
+
+  Widget _dateTimeInfo() {
+    DateTime now = _weather!.date!;
+    return Column(
+      children: [
+        Text(
+          DateFormat("h:mm a").format(now),
+          style: const TextStyle(
+            fontSize: 35,
+          ),
+        ),
+        const SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("EEEE").format(now),
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              "  ${DateFormat("d.m").format(now)}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png",
+              ),
+            ),
+          )
+        ),
+        Text(_weather?.weatherDescription ?? "",
+            style: const TextStyle(fontSize: 20,)),
+      ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text(
+      "${_weather?.temperature?.celsius?.toStringAsFixed(0)}° C",
+      style: const TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
     );
   }
 }
